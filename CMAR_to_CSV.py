3ab623a3d33c13081afb48ad0b10bd64267e4a33
Brad Covey <bradcovey@gmail.com>
2021-03-17 13:44:27 -0300

    Fixes bug with row merging, adds ability to append new data

--- CMAR_to_CSV.py
+++ CMAR_to_CSV.py
@@ -1,15 +1,41 @@
 # Import libraries
+import os
 import pandas as pd
 from sodapy import Socrata
 
-def fetch_data(dataset_id):
+def fetch_data(dataset_id, output_raw_csv=False, raw_csv_filename=None):
+    limit = 6000000
     client = Socrata("data.novascotia.ca", 'cx4XloH6tDgX8ZhFFmtAuxtMc')
-   
-    results = client.get(dataset_id, limit=6000000)
+
+    if raw_csv_filename is None:
+        raw_csv_filename = "%s_raw.csv" % dataset_id
+
+    results = None
+    existing_df = None
+    if os.path.exists(raw_csv_filename):
+        existing_df = pd.read_csv(raw_csv_filename, parse_dates=['timestamp'])
+        max_date = existing_df['timestamp'].max()
+        max_date_string = max_date.strftime('%Y-%m-%dT%H:%M:%S.%f')
+        results = client.get(dataset_id, where="timestamp > \"%s\"" % max_date_string, limit=limit)
+    else:
+        results = client.get(dataset_id, limit=limit)
     # results = client.get("eb3n-uxcb", limit=500000)
+
 
     # Convert to pandas DataFrame
     results_df = pd.DataFrame.from_records(results)
+
+    new_rows = len(results_df)
+    if existing_df is not None:
+        results_df = results_df.append(existing_df)
+
+    # Output the unmerged data if desired
+    if output_raw_csv and new_rows > 0:
+        print("Found new data, writing to raw file: %s" % raw_csv_filename)
+        results_df.to_csv(raw_csv_filename, index=False)
+
+    if new_rows == 0:
+        print("No new data found, proceeding with data from: %s" % raw_csv_filename)
     return results_df
 
 def merge_timestamps(grouped_df):
@@ -27,10 +53,11 @@ def merge_timestamps(grouped_df):
     # If there are multiple, different values for the same variable, at the same time, raise an error     
     sci_variable_list = grouped_df['variable'].unique()
     for variable in sci_variable_list:
-        if len(grouped_df['value'].unique()) > 1:
+        rows_for_variable = grouped_df[grouped_df['variable'] == variable]
+        if len(rows_for_variable['value'].unique()) > 1:
             raise(ValueError("Multiple values for the same variable at timestamp: %s" % new_df['timestamp']))
         else:
-            new_df[variable] = grouped_df['value'].unique()
+            new_df[variable] = rows_for_variable['value'].unique()
     return new_df
 
 def group_by_timestamp(df):
@@ -45,12 +72,7 @@ def group_by_timestamp(df):
     return merged_df
 
 def main(dataset_id="eb3n-uxcb", merged_csv_filename=None, output_raw_csv=False, raw_csv_filename=None):
-    df = fetch_data(dataset_id)
-    # Output the unmerged data if desired
-    if output_raw_csv:
-        if raw_csv_filename is None:
-            raw_csv_filename = "%s_raw.csv" % dataset_id
-        df.to_csv(raw_csv_filename, index=False)
+    df = fetch_data(dataset_id, output_raw_csv, raw_csv_filename)
 
     # Merge data based on waterbody, station, lease, latitude, longitude, deployment_period, timestamp, and sensor 
     merged_df = group_by_timestamp(df)
