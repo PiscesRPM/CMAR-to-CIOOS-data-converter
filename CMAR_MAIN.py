759d1ba16e750eb647cb69626b5c832d6a5d1962
cruncher19 <bradcovey@gmail.com>
2021-05-17 13:58:28 -0300

    Adds example showing how to call CMAR_fetch_data from main

--- CMAR_MAIN.py
+++ CMAR_MAIN.py
@@ -5,12 +5,15 @@ import sys
 import argparse
 from pathlib import Path
 
+from data_tools import CMAR_fetch_data
+
 
 def main():
     # example list
     dataset_id_list = ['eb3n-uxcb','x9dy-aai9']
     for set_id in dataset_id_list:
-        os.system("CMAR_fetchData.py ")
+        # os.system("CMAR_fetchData.py ")
+        # CMAR_fetch_data.main() <--- call like this
         outputFolder = os.path.dirname(__file__) + '/' + set_id + ' Data Set'
         path = os.path.join(os.path.dirname(__file__), outputFolder)
         # Path(path).mkdir(parents=True, exist_ok=True)
