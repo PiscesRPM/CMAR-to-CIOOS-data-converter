11a683d11437017170ce09de5f998367d0190248
cruncher19 <bradcovey@gmail.com>
2021-05-20 12:40:12 -0300

    Refactors handling of files and folders for CMAR_fetch_data.py

--- CMAR_MAIN.py
+++ CMAR_MAIN.py
@@ -4,19 +4,54 @@ from sodapy import Socrata
 import sys
 import argparse
 from pathlib import Path
+import argparse
 
 from data_tools import CMAR_fetch_data, CSV_split_by_station, CMAR_data_converter, CMAR_fetch_metadata
 
+def setup_raw_output_filename(dataset_output_directory, dataset_id):
+    """Builds a pathname for a raw output file, creates directories as needed
 
-def main():
+    Args:
+        dataset_output_directory (String): The output directory for the overall dataset
+        dataset_id (String): The dataset ID for the current dataset
+
+    Returns:
+        String: The filename including path for the raw output file
+    """    
+    # Create the dataset_output_directory if it doesn't exist
+    if not(os.path.exists(dataset_output_directory)):
+        os.mkdir(dataset_output_directory)
+    
+    # Create an output filename for the raw data, including the path to the directory
+    raw_output_filename = os.path.join(
+        dataset_output_directory,
+        '%s_raw.csv' % dataset_id
+    )
+    return raw_output_filename
+
+def main(output_directory):
     # example list
     dataset_id_list = ['eb3n-uxcb','x9dy-aai9']
-    for set_id in dataset_id_list:
-        # CMAR_fetch_data.main(set_id, True)
-        raw_file_name = 'data_tools\%s\%s' % (set_id,set_id + '_raw.csv')
-        CSV_split_by_station.main(raw_file_name,set_id)
+    for dataset_id in dataset_id_list:
+        # Create a new folder inside of output_directory for each dataset
+        dataset_output_directory = os.path.join(output_directory, dataset_id)
+
+        print("Fetching data for dataset: %s" % dataset_id)
+        raw_output_filename = setup_raw_output_filename(
+            dataset_output_directory,
+            dataset_id
+        )
+        CMAR_fetch_data.main(dataset_id, raw_output_filename)
+        # raw_file_name = 'data_tools\%s\%s' % (set_id,set_id + '_raw.csv')
+        # CSV_split_by_station.main(raw_file_name,set_id)
 
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("output_directory", type=str,
+                    help="Output directory for data and metadata")
+    args = parser.parse_args()
+    output_directory = args.output_directory
+    main(output_directory)
--- data_tools/CMAR_fetch_data.py
+++ data_tools/CMAR_fetch_data.py
@@ -4,24 +4,23 @@ from sodapy import Socrata
 import sys
 import argparse
 
-def fetch_data(dataset_id, output_raw_csv=False, raw_csv_filename=None):
-    
-    
+def fetch_data(dataset_id, output_filename):
+    """Fetch data from the open data Nova Scotia portal and write to a CSV
+
+    Args:
+        dataset_id (String):    The id of a dataset in the nova scotia open data portal, this is normally found in the url for the dataset 
+        output_filename (String): The name of the file where the dataset will be stored
+
+    Returns:
+        pandas.DataFrame: A Pandas DataFrame containing the retreived data
+    """    
     limit = 6000000
     client = Socrata("data.novascotia.ca", 'cx4XloH6tDgX8ZhFFmtAuxtMc')
 
-    if raw_csv_filename is None:
-        raw_csv_filename = "%s_raw.csv" % dataset_id
-
-    if output_raw_csv:
-        output_folder = os.path.dirname(__file__) + '/' + dataset_id
-        path = os.path.join(os.path.dirname(__file__), output_folder)
-        raw_csv_filename = os.path.join(raw_csv_filename)
-
     results = None
     existing_df = None
-    if os.path.exists(raw_csv_filename):
-        existing_df = pd.read_csv(raw_csv_filename, parse_dates=['timestamp'])
+    if os.path.exists(output_filename):
+        existing_df = pd.read_csv(output_filename, parse_dates=['timestamp'])
         max_date = existing_df['timestamp'].max()
         max_date_string = max_date.strftime('%Y-%m-%dT%H:%M:%S.%f')
         results = client.get(dataset_id, where="timestamp > \"%s\"" % max_date_string, limit=limit)
@@ -38,33 +37,29 @@ def fetch_data(dataset_id, output_raw_csv=False, raw_csv_filename=None):
         results_df = results_df.append(existing_df)
 
     # Output the unmerged data if desired
-    if output_raw_csv and new_rows > 0:
-        print("Found new data, writing to raw file: %s" % raw_csv_filename)
+    if new_rows > 0:
+        print("Found new data, writing to raw file: %s" % output_filename)
 
-        if not os.path.exists(path):
-            os.mkdir(path)
-        results_df.to_csv(raw_csv_filename, index=False)
+        results_df.to_csv(output_filename, index=False)
 
     if new_rows == 0:
-        print("No new data found, proceeding with data from: %s" % raw_csv_filename)
+        print("No new data found, proceeding with data from: %s" % output_filename)
     return results_df
 
-def main(dataset_id=None, output_raw_csv=False, raw_csv_filename=None):
-    df = fetch_data(dataset_id, output_raw_csv, raw_csv_filename)
+def main(dataset_id, output_filename):
+    df = fetch_data(dataset_id, output_filename)
+    return df
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("SetID", type=str,
+    parser.add_argument("dataset_id", type=str,
                     help="Dataset ID")
-    parser.add_argument("-o", help="custom name of output file")
+    parser.add_argument("output_filename", help="custom name of output file")
     args = parser.parse_args()
-    if args.o:
-        output_raw_csv = args.o
-        print("Custom name %s" %args.o)
-    else:
-        output_raw_csv = None
-    dataset_id = args.SetID
 
-    main(dataset_id=dataset_id, output_raw_csv=True, raw_csv_filename=output_raw_csv)
+    dataset_id = args.dataset_id
+    output_filename = args.output_filename
+
+    main(dataset_id, output_filename)
